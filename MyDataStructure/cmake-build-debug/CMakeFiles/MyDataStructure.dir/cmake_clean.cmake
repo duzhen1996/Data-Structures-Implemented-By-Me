c3d630b22dde09665ba267f02dabdf0f03c47b2a
ZhenDu <454858191@qq.com>
2017-03-28 22:05:12 +0800

    2017-03-28

---
+++ MyDataStructure/cmake-build-debug/CMakeFiles/MyDataStructure.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyDataStructure.dir/main.cpp.o"
+  "CMakeFiles/MyDataStructure.dir/KMP.cpp.o"
+  "MyDataStructure.pdb"
+  "MyDataStructure"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyDataStructure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ MyDataStructure/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MyDataStructure
+//
+//  Created by 杜臻 on 17/3/27.
+//  Copyright © 2017年 杜臻. All rights reserved.
+//
+
+#include <iostream>
+#include "Queue.h"
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+
+
+    return 0;
+}
